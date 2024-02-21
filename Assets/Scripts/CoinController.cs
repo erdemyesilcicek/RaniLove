dc320fcb91fe848d6dd0579403dd6476e0743d19
Erdem YEŞİLÇİÇEK <erdmyslcck9@outlook.com>
2024-02-21 20:27:15 +0300

    its over..

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -6,13 +6,14 @@ using UnityEngine.UI;
 public class CoinController : MonoBehaviour
 {
     [SerializeField] Text scoreValueText;
+    [SerializeField] int coinValue;
     private void Update() => transform.Rotate(new Vector3(0f, 0.5f, 0f));
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             int scorevalue = int.Parse(scoreValueText.text);
-            scorevalue += 25;
+            scorevalue += coinValue;
             scoreValueText.text = scorevalue.ToString();
             Destroy(gameObject);
         }
